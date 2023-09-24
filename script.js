6d8bae5f29623d8f978ac272a9a142741351a9a1
kristihu <44356817+kristihu@users.noreply.github.com>
2023-09-24 20:35:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function updateImageSource() {
   const screenWidth = window.innerWidth;
 
   if (screenWidth >= 800) {
-    imageElement.src = '/assets/images/illustration-sign-up-desktop.svg';
+    imageElement.src = './assets/images/illustration-sign-up-desktop.svg';
   } else {
-    imageElement.src = '/assets/images/illustration-sign-up-mobile.svg';
+    imageElement.src = './assets/images/illustration-sign-up-mobile.svg';
   }
 }
 
